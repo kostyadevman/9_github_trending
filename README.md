235bef2db4a58035d607e05e28dfb0f3871fe623
dolly <timohakt@yandex.ru>
2018-03-02 02:35:50 +0300

    Update README and list of requirements Change get_trending_repositories function

--- README.md
+++ README.md
@@ -1,7 +1,39 @@
 # Github Trends
 
-[TODO. There will be project description]
+# Quickstart
 
+The program finds repositories on Github created in the previous week, takes 20 high-starred of them and counts the amount of open issues. It outputs the repository name, number of issues and a url to the repo
+
+The script requires the installed Python interpreter version 3.5 and library **requests**
+
+
+
+```bash
+$ pip install -r requirements.txt
+```
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+$ python github_trending.py
+
+Repository: hangzhou_house_knowledge
+Url: https://github.com/houshanren/hangzhou_house_knowledge
+Issues: 2
+
+Repository: requests-html
+Url: https://github.com/kennethreitz/requests-html
+Issues: 2
+
+Repository: 30-seconds-of-css
+Url: https://github.com/atomiks/30-seconds-of-css
+Issues: 2
+
+Repository: mpvue
+Url: https://github.com/Meituan-Dianping/mpvue
+Issues: 2
+...
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,9 +1,18 @@
 import requests
+from datetime import datetime, timedelta
 
 
 def get_trending_repositories(top_size):
+    week_ago = datetime.now() - timedelta(weeks=1)
+    request_payload = {
+        'q': 'created:>{}'.format(week_ago.strftime('%Y-%m-%d')),
+        'sort': 'stars',
+        'order': 'des'
+
+    }
     github_response = requests.get(
-        'https://api.github.com/search/repositories?q=created:%3E2018-03-01&sort=stars&order=des'
+        'https://api.github.com/search/repositories', params=request_payload
+
     )
     github_repositories = github_response.json()
     return github_repositories['items'][:top_size]
@@ -12,15 +21,15 @@ def get_trending_repositories(top_size):
 def get_open_issues_amount(repo_owner, repo_name):
     github_response = requests.get(
         'https://api.github.com/repos/{}/{}/issues'.format(
-            repo_name,
-            repo_owner
+            repo_owner,
+            repo_name
         )
     )
     return len(github_response.json())
 
 
 if __name__ == '__main__':
-    top_size = 2
+    top_size = 20
     trending_repositories = get_trending_repositories(top_size)
     for repository in trending_repositories:
         repo_name = repository['name']
@@ -28,4 +37,8 @@ if __name__ == '__main__':
         repo_url = repository['html_url']
         stars_count = repository['stargazers_count']
         issues_amount = get_open_issues_amount(repo_owner, repo_name)
-
+        print('Repository: {}\nUrl: {}\nIssues: {}\n'.format(
+            repo_name,
+            repo_url,
+            issues_amount
+        ))
