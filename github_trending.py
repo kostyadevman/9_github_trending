b720c200f9394b2b7bec1666267c87fe37306181
dolly <timohakt@yandex.ru>
2018-03-02 05:11:39 +0300

    Update get_trending_repositories function and Update get_open_issues_amount function and update main function

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,31 @@
+import requests
 
 
 def get_trending_repositories(top_size):
-    pass
+    github_response = requests.get(
+        'https://api.github.com/search/repositories?q=created:%3E2018-03-01&sort=stars&order=des'
+    )
+    github_repositories = github_response.json()
+    return github_repositories['items'][:top_size]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    github_response = requests.get(
+        'https://api.github.com/repos/{}/{}/issues'.format(
+            repo_name,
+            repo_owner
+        )
+    )
+    return len(github_response.json())
+
 
 if __name__ == '__main__':
-    pass
+    top_size = 2
+    trending_repositories = get_trending_repositories(top_size)
+    for repository in trending_repositories:
+        repo_name = repository['name']
+        repo_owner = repository['owner']['login']
+        repo_url = repository['html_url']
+        stars_count = repository['stargazers_count']
+        issues_amount = get_open_issues_amount(repo_owner, repo_name)
+
